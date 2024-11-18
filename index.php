8c3e87397cbcaa1d94987cafbb60b8bb692946da
Andrea Hunčíková <andhun01@easv365.dk>
2024-11-18 13:09:26 +0100

    changes

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<?php get_header(); ?>
+
 <?php
 echo "Miro A Danica a peta su slapky a INGRID TIEZ napisala danica!";
 echo "Mmisko";
+echo '<img src="https://upload.wikimedia.org/wikipedia/en/0/03/MiskoCompanyLogo.png" alt="My Image" />';
 ?>
+
+<?php get_footer(); ?>
