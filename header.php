29b61d218f74d441bec6f315f360b956eb3e8b71
Andrea Hunčíková <andhun01@easv365.dk>
2024-11-19 19:32:10 +0100

    changes

--- header.php
+++ header.php
@@ -10,10 +10,7 @@
 <body <?php body_class(); ?>>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-<!-- Left: Logo -->
-        <a class="navbar-brand" href="<?php echo home_url(); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="<?php bloginfo('name'); ?>" style="height: 40px;">
-        </a>
+
             <!-- Hamburger toggle -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
