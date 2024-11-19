f0340f1eb2d6b602e358575a6bd9ebc6764a964b
Andrea Hunčíková <andhun01@easv365.dk>
2024-11-19 20:11:21 +0100

    changes

--- header.php
+++ header.php
@@ -10,7 +10,17 @@
 <body <?php body_class(); ?>>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-
+            <!-- Left: Logo -->
+            <a class="navbar-brand" href="<?php echo home_url(); ?>">
+                <?php
+                // Display custom logo if set, otherwise show site name
+                if (has_custom_logo()) {
+                    the_custom_logo();
+                } else {
+                    bloginfo('name');
+                }
+                ?>
+            </a>
             <!-- Hamburger toggle -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
