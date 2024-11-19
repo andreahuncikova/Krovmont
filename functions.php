1f591b709b84b51ba854877ef707bce3647a6eef
Andrea Hunčíková <andhun01@easv365.dk>
2024-11-19 16:55:12 +0100

    changes

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 function krovmont_load_resources() {
-    wp_enqueue_style("bootstrap-css", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css");
+    wp_enqueue_style("bootstrap-css", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css");
     wp_enqueue_style("theme-style", get_template_directory_uri() . "/style.css");
 }
 add_action("wp_enqueue_scripts" , "krovmont_load_resources");
