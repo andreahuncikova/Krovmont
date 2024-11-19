fa2310ccb8c09b347a8ab38cd2dc8ea7fc8ebd75
Andrea Hunčíková <andhun01@easv365.dk>
2024-11-19 18:22:53 +0100

    fix font

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php 
-// Enqueue Bootstrap and theme styles
 function krovmont_load_resources() {
+    // Google Fonts
     wp_enqueue_style("google-fonts-montserrat", "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap", [], null);
+
+    // Bootstrap
     wp_enqueue_style("bootstrap-css", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css");
-    wp_enqueue_style("theme-style", get_template_directory_uri() . "/style.css");
+
+    // Theme Styles
+    wp_enqueue_style("theme-style", get_stylesheet_uri(), ['bootstrap-css', 'google-fonts-montserrat'], null);
 }
 add_action("wp_enqueue_scripts", "krovmont_load_resources");
 
